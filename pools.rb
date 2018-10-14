ea711413545c988fe52e55a135c5225ad672c27b
monerorus <32051077+monerorus@users.noreply.github.com>
2018-10-14 13:35:37 +0000

    cleanup pools list

--- pools.rb
+++ pools.rb
@@ -13,39 +13,28 @@ class Pools
   #some strange pools
     #"teracycle.net" => [2, "http://teracycle.net:8117"], #ALSO minemonero.gq, dead
     #"alimabi.cn" => [2, "http://118.190.133.167:81"], #dead
+  
 
   @@pools_api_base_url = { 
     "xmrpool.net" => [1, "https://api.xmrpool.net"], 
     "xmrpool.eu" => [2,"https://web.xmrpool.eu:8119"],
     "supportxmr.com" => [1, "https://supportxmr.com/api"],
     "xmr.nanopool.org" => [0, "https://xmr.nanopool.org/api/v1/load_account"],
-    "mixpools.org" => [2, "https://mixpools.org:8117"],
-    "viaxmr.com" => [1, "https://api.viaxmr.com"],
     "monero.hashvault.pro" => [1, "https://monero.hashvault.pro/api"],
     "moriaxmr.com" => [1, "https://api.moriaxmr.com"],
     "moneroocean.stream" => [1, "https://api.moneroocean.stream"],
-    "monero.lindon-pool.win" => [1, "https://monero.lindon-pool.win/api"],
     "xmrpool.de" => [1, "http://pool.xmrpool.de/api"],
-    "xmr.poolto.be" => [2, "https://xmr.poolto.be:8120"],
     "minexmr.com" => [2, "http://api.minexmr.com:8080"],
-    "xmr.prohash.net" => [2, "http://xmr.prohash.net:8117"],
     "xmr.mypool.online" => [1, "https://api.xmr.mypool.online"],
     "bohemianpool.com" => [1, "https://bohemianpool.com/api"],
-    "moneropool.com" => [2, "https://api.moneropool.com"],
+    "moneropool.com" => [1, "https://api.moneropool.com/api"],
     "moneropool.nl" => [1, "https://moneropool.nl/api"],
-    "iwanttoearn.money" => [2, "http://iwanttoearn.money:8117"],
     "pool.xmr.pt" => [1, "https://pool.xmr.pt/api"],
     "monero.crypto-pool.fr" => [2, "https://monero.crypto-pool.fr:8091"],
     "pool.miners.pro" => [2, "http://pool.miners.pro:8117"],
-    "cryptmonero.com" => [2, "http://46.165.232.77:8117"],
-    "monero.us.to" => [1, "http://monero.gt/api"],
+    "monero.gt" => [1, "https://monero.gt/api"],
     "monerohash.com" => [2, "https://monerohash.com/api"], #long responce from api.
-    "usxmrpool.com" => [1, "https://api.usxmrpool.com/api"],
-    "xmrpool.xyz" => [1, "https://api.xmrpool.xyz"],
-    "pooldd.com" => [2, "http://minexmr.pooldd.com:8080"],
-    "monero.riefly.id" => [2, "https://xmr.riefly.id:8119"],
-    "ratchetmining.com" => [1, "https://ratchetmining.com/api"],
-    "secumine.net" => [1, "https://secumine.net/api"],
+    "pooldd.com" => [2, "https://minexmr.pooldd.com:8119"],
     "multipooler.com" => [2, "https://multipooler.com:8119"],
     "mine.pro" => [1, "https://xmr.mine.pro/api"],
     "monero.miners.pro" => [2, "https://apimonero.miners.pro"],
